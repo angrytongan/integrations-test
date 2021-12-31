7a2589856ad54f2721238a70128764d35a5a59ef
Dean Fogarty <angrytongan@gmail.com>
2021-12-31 15:31:16 +1000

    Add MDN basic validation.

--- foo.js
+++ foo.js
@@ -1,5 +1,6 @@
 const EMAIL_RE = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const EMAIL_NEW_RE = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const EMAIL_SONARCLOUD_REC = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const EMAIL_MDN = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 
 const tests = [
     { address: 'foo@example.com', valid: true },
@@ -18,15 +19,16 @@ const results = [];
 
 tests.forEach((t) => {
     const a = EMAIL_RE.test(t.address);
-    const b = EMAIL_NEW_RE.test(t.address);
+    const b = EMAIL_SONARCLOUD_REC.test(t.address);
+    const c = EMAIL_MDN.test(t.address);
 
     results.push({
+        address: t.address,
         expected: t.valid,
         'old RE': a,
-        'new RE': b,
-        'old RE match': a === t.valid,
-        'new RE match': b === t.valid,
-        'test pass': a === b && a === t.valid,
+        'SonarCloud RE': b,
+        'MDN RE': c,
+        'test pass': a === b && a === c && a === t.valid,
     });
 });
 
