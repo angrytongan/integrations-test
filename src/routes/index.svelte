d9c270400c3fba57ac0504eb14bc5a142f851416
Dean Fogarty <angrytongan@gmail.com>
2022-01-01 08:10:16 +1000

    Install svelte app (#3)
    * Bring in a sveltekit app.
    
    * Add some stuff.

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    let count: number = 0;
+
+    const increment = () => {
+        count++;
+    };
+</script>
+
+<div>
+    <button type="button" on:click={increment}>{count}</button>
+</div>
+
+<style>
+    div {
+        width: 700px;
+        margin: 0 auto;
+        text-align: center;
+    }
+    button {
+        font-size: 50px;
+    }
+</style>
