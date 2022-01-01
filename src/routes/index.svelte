b235966c35fc7fd911773cb1ae62c52feab5a38b
Dean Fogarty <angrytongan@gmail.com>
2022-01-01 11:48:28 +1000

    Fixing / messing around.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,33 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
+
+    let now: string = new Date();
     let count: number = 0;
+    let text: string = '';
 
     const increment = () => {
         count++;
     };
+
+    onMount(() => {
+        const intervalId = setInterval(() => {
+            now = new Date();
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    });
+
 </script>
 
 <div>
-    <button type="button" on:click={increment}>{count}</button>
+    <button type="button" on:click={increment} data-cy="click-me">Click me</button>
+    <input type="text" bind:value={text} placeholder="Some text" data-cy="text" />
+
+    <p>{now}</p>
+    <p data-cy="paragraph-count">{count}</p>
+    <p data-cy="paragraph-text">{text}</p>
 </div>
 
 <style>
@@ -15,8 +35,5 @@
         width: 700px;
         margin: 0 auto;
         text-align: center;
-    }
-    button {
-        font-size: 50px;
     }
 </style>
