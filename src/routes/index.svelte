e838980809f2b2ce6126562691f09e4ef7ba20bf
Dean Fogarty <angrytongan@gmail.com>
2022-01-31 17:54:36 +1000

    Improve waiting for page ready.
    Fixes #22.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,30 @@
+<script lang="ts" context="module">
+    export const load = ({}) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve({});
+            }, 3000);
+        });
+    };
+</script>
+
 <script lang="ts">
     import { onMount } from 'svelte';
 
-    let now: string = new Date();
+    let now: string = new Date().toString();
     let count: number = 0;
     let text: string = '';
+    let loading: boolean = true;
 
     const increment = () => {
         count++;
     };
 
     onMount(() => {
+        loading = false;
+
         const intervalId = setInterval(() => {
-            now = new Date();
+            now = new Date().toString();
         }, 1000);
 
         return () => {
@@ -24,6 +37,10 @@
 <div>
     <button type="button" on:click={increment} data-cy="click-me">Click me</button>
     <input type="text" bind:value={text} placeholder="Some text" data-cy="text" />
+
+    {#if loading}
+        <p data-cy="loading">loading</p>
+    {/if}
 
     <p>{now}</p>
     <p data-cy="paragraph-count">{count}</p>
